2ad00f582d12152ae5dab7fd2674c8b18ce42464
Rajesh Khuntia <rajeshk001@icloud.com>
2023-10-13 09:23:18 +0530

    Initial Commit

---
+++ BlackTickTick/BlackTickTickApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackTickTickApp.swift
+//  BlackTickTick
+//
+//  Created by Rajesh Khuntia on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BlackTickTickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlackTickTick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlackTickTick
+//
+//  Created by Rajesh Khuntia on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
